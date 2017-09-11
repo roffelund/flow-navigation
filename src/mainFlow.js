38592b328f5fb4ea7d9ba47b0fe13abb1208046c
Gustav <gustav.haglund@gmail.com>
2017-09-11 14:01:16 +0200

    Fix import error

--- src/mainFlow.js
+++ src/mainFlow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import ContentAnimation from './components/contentAnimation';
+import ContentAnimation from './contentAnimation';
 
 function renderContentPage(
   { title, startPosition, ContentComponent, currentPosition, injected },
